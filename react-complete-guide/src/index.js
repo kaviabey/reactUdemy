b393bcad9f2a3a69044db8f07f0e90ae7615d030
Kavisha Chiranjaya Abeynayaka <87388691+kaviabey@users.noreply.github.com>
2022-06-03 08:37:10 +0530

    Add files via upload

---
+++ react-complete-guide/src/index.js
@@ -0,0 +1,7 @@
+import ReactDOM from 'react-dom/client';
+
+import './index.css';
+import Apps from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Apps />);
